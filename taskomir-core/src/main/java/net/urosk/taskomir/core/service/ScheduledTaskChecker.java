0e0815dfe230ddfebc70f5bffc0be54d3bc92ce9
Uroš Kristan <uros.kristan@gmail.com>
2025-03-12 18:16:57 +0100

    minimal

--- taskomir-core/src/main/java/net/urosk/taskomir/core/service/ScheduledTaskChecker.java
+++ taskomir-core/src/main/java/net/urosk/taskomir/core/service/ScheduledTaskChecker.java
@@ -53,6 +53,8 @@ public class ScheduledTaskChecker {
      */
     @Scheduled(fixedDelayString = "#{@taskomirProperties.cleanupInterval.toMillis()}")
     public void cleanupOldTasks() {
+
+        log.info("Cleaning up old tasks");
         long now = System.currentTimeMillis();
 
         // 1) Avtomatsko prepis SUCCEEDED => DELETED po X sekundah
@@ -83,6 +85,8 @@ public class ScheduledTaskChecker {
      */
     @Scheduled(fixedDelayString = "#{@taskomirProperties.scheduledCheckInterval.toMillis()}")
     public void checkScheduledTasks() {
+
+        log.info("Checking scheduled tasks...");
         long nowMillis = System.currentTimeMillis();
         Instant nowInstant = Instant.ofEpochMilli(nowMillis);
 
